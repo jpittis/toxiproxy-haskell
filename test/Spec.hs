07109a348c60a94ef5410992bea17e300c6cb58c
Jake Pittis <jakepittis@gmail.com>
2018-03-04 15:10:46 -0500

    spin up a toxiproxy server with each test

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,8 +7,17 @@ import Servant.Client
 import Servant.API
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import qualified Data.Map.Strict as Map
+import Control.Concurrent (threadDelay)
+import System.Process (withCreateProcess, proc, CreateProcess)
 
 import Toxiproxy
+
+withToxiproxyServer :: IO a -> IO a
+withToxiproxyServer f =
+  withCreateProcess server $ \_ _ _ _ -> threadDelay 100000 >> f
+  where
+    server :: CreateProcess
+    server = proc "toxiproxy-server" []
 
 toxiproxyUrl = BaseUrl Http "localhost" 8474 ""
 
@@ -20,21 +29,25 @@ main :: IO ()
 main = hspec $
   describe "Toxiproxy" $ do
     it "gets version" $
-      run getVersion `shouldReturn` Right "git-fe6bf4f"
+      withToxiproxyServer $
+        run getVersion `shouldReturn` Right "git-fe6bf4f"
     it "posts reset" $
-      run postReset `shouldReturn` Right NoContent
+      withToxiproxyServer $
+        run postReset `shouldReturn` Right NoContent
     it "gets proxies" $
-      run getProxies `shouldReturn` Right Map.empty
-    it "creates and gets a proxy" $ do
-      let name = "myProxy"
-      let proxy = Proxy
-            { proxyName     = name
-            , proxyListen   = "localhost:4444"
-            , proxyUpstream = "lcoalhost:4445"
-            , proxyEnabled  = False
-            , proxyToxics   = []
-            }
-      run (createProxy proxy) `shouldReturn` Right proxy
-      run getProxies `shouldReturn` Right (Map.fromList [(name, proxy)])
-      run (getProxy name) `shouldReturn` Right proxy
+      withToxiproxyServer $
+        run getProxies `shouldReturn` Right Map.empty
+    it "creates and gets a proxy" $
+      withToxiproxyServer $ do
+        let name = "myProxy"
+        let proxy = Proxy
+              { proxyName     = name
+              , proxyListen   = "localhost:4444"
+              , proxyUpstream = "lcoalhost:4445"
+              , proxyEnabled  = False
+              , proxyToxics   = []
+              }
+        run (createProxy proxy) `shouldReturn` Right proxy
+        run getProxies `shouldReturn` Right (Map.fromList [(name, proxy)])
+        run (getProxy name) `shouldReturn` Right proxy
 
